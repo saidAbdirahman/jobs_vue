56b9c105f8d1e8f13c20130d30c29432bdfbb708
saidAbdirahman <saidqaaje11@gmail.com>
2024-07-23 16:13:09 +0200

    intial commit
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{name:'home'}">Home</router-link> |
+    <router-link :to="{name:'about'}">About</router-link>
   </nav>
   <router-view/>
+  <footer>
+    @copyright by abdalla
+  </footer>
 </template>
 
 <style>
@@ -22,9 +25,12 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background: crimson;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Homepage</h1>
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum quia cupiditate ducimus obcaecati recusandae cumque assumenda mollitia. Illo hic blanditiis dignissimos dolorum beatae veritatis ipsa voluptatibus assumenda eos quis similique, autem deserunt mollitia omnis praesentium sit temporibus laudantium? Illum repellendus deserunt natus dolor dolore omnis tempore et hic est. Voluptates animi, cupiditate consequuntur culpa accusantium numquam deleniti omnis. Culpa id at fuga laborum voluptate sit excepturi quidem labore! Laudantium expedita quidem facilis fugiat, consectetur maxime optio non vitae sint. Fugiat perferendis hic praesentium ea officiis obcaecati error quaerat blanditiis, necessitatibus incidunt dolores inventore provident, tempora aliquid adipisci illo officia.</p>
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam nostrum quia cupiditate ducimus obcaecati recusandae cumque assumenda mollitia. Illo hic blanditiis dignissimos dolorum beatae veritatis ipsa voluptatibus assumenda eos quis similique, autem deserunt mollitia omnis praesentium sit temporibus laudantium? Illum repellendus deserunt natus dolor dolore omnis tempore et hic est. Voluptates animi, cupiditate consequuntur culpa accusantium numquam deleniti omnis. Culpa id at fuga laborum voluptate sit excepturi quidem labore! Laudantium expedita quidem facilis fugiat, consectetur maxime optio non vitae sint. Fugiat perferendis hic praesentium ea officiis obcaecati error quaerat blanditiis, necessitatibus incidunt dolores inventore provident, tempora aliquid adipisci illo officia.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
